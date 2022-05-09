b3f929b1414f3394255252d928803640c398e2c1
traacc <vaska.i.88@gmail.com>
2022-05-09 15:31:22 +0300

    feat: More complited english layout

--- scripts.js
+++ scripts.js
@@ -5,71 +5,78 @@ class Keyboard {
     }
     addLayout() {
         return {
-            tilda: {default:'`', shift:'~', altgr: '', shift_altgr: ''},
-            1: {default:'1', shift:'!', altgr: '', shift_altgr: ''},
-            2: {default:'2', shift:'@', altgr: '', shift_altgr: ''},
-            3: {default:'3', shift:'#', altgr: '', shift_altgr: ''},
-            4: {default:'4', shift:'$', altgr: '', shift_altgr: ''},
-            5: {default:'5', shift:'%', altgr: '', shift_altgr: ''},
-            6: {default:'6', shift:'^', altgr: '', shift_altgr: ''},
-            7: {default:'7', shift:'&', altgr: '', shift_altgr: ''},
-            8: {default:'8', shift:'*', altgr: '', shift_altgr: ''},
-            9: {default:'9', shift:'(', altgr: '', shift_altgr: ''},
-            0: {default:'0', shift:')', altgr: '', shift_altgr: ''},
-            minus: {default:'-', shift:'_', altgr: '', shift_altgr: ''},
-            eq: {default:'=', shift:'+', altgr: '', shift_altgr: ''},
-            backspace: {default: '', type: 'bksp', width: 'bksp'},
-            tab: {default: '', type: 'tab', width: 'tab'},
-            q: {default:'q', shift:'Q', altgr: '', shift_altgr: ''},
-            w: {default:'w', shift:'W', altgr: '', shift_altgr: ''},
-            e: {default:'e', shift:'E', altgr: '', shift_altgr: ''},
-            r: {default:'r', shift:'R', altgr: '', shift_altgr: ''},
-            t: {default:'t', shift:'T', altgr: '', shift_altgr: ''},
-            y: {default:'y', shift:'Y', altgr: '', shift_altgr: ''},
-            u: {default:'u', shift:'U', altgr: '', shift_altgr: ''},
-            i: {default:'i', shift:'I', altgr: '', shift_altgr: ''},
-            o: {default:'o', shift:'O', altgr: '', shift_altgr: ''},
-            p: {default:'p', shift:'P', altgr: '', shift_altgr: ''},
-            open_square: {default:'[', shift:'{', altgr: '', shift_altgr: ''},
-            close_square: {default:']', shift:'}', altgr: '', shift_altgr: ''},
-            backslash: {default:'\\', shift:'|', altgr: '', shift_altgr: ''},
-            caps: {default:'', type: 'caps', width:'caps'},
-            a: {default:'a', shift:'A', altgr: '', shift_altgr: ''},
-            s: {default:'s', shift:'S', altgr: '', shift_altgr: ''},
-            d: {default:'d', shift:'D', altgr: '', shift_altgr: ''},
-            f: {default:'f', shift:'F', altgr: '', shift_altgr: ''},
-            g: {default:'g', shift:'G', altgr: '', shift_altgr: ''},
-            h: {default:'h', shift:'H', altgr: '', shift_altgr: ''},
-            j: {default:'j', shift:'J', altgr: '', shift_altgr: ''},
-            k: {default:'k', shift:'K', altgr: '', shift_altgr: ''},
-            l: {default:'l', shift:'L', altgr: '', shift_altgr: ''},
-            semicolon: {default:';', shift:':', altgr: '', shift_altgr: ''},
-            apostrophe: {default:'\'', shift:'\"', altgr: '', shift_altgr: ''},
-            enter: {default:'', type: 'enter', width:'enter'},
-            shift: {default:'', type: 'shift', width:'shift'},
-            z: {default:'z', shift:'Z', altgr: '', shift_altgr: ''},
-            x: {default:'x', shift:'X', altgr: '', shift_altgr: ''},
-            c: {default:'c', shift:'C', altgr: '', shift_altgr: ''},
-            v: {default:'v', shift:'V', altgr: '', shift_altgr: ''},
-            b: {default:'b', shift:'B', altgr: '', shift_altgr: ''},
-            n: {default:'n', shift:'N', altgr: '', shift_altgr: ''},
-            m: {default:'m', shift:'M', altgr: '', shift_altgr: ''},
-            comma: {default:',', shift:'<', altgr: '', shift_altgr: ''},
-            period: {default:'.', shift:'>', altgr: '', shift_altgr: ''},
-            slash: {default:'/', shift:'?', altgr: '', shift_altgr: ''},
-            ctrl: {default:'ctrl', func:'ctrl', shift:'?', altgr: '', shift_altgr: ''},
-            os: {default:'os', func:'os', altgr: '', shift_altgr: ''},
-            alt: {default:'alt', func:'alt', altgr: '', shift_altgr: ''},
-            space: {default:'space', func:'space', altgr: '', shift_altgr: '', width: "space"},
-            altgr: {default:'Alt Gr', func:'altgr', altgr: '', shift_altgr: ''}
+            tilda: {default:'`', shift:'~', altgr: '', shift_altgr: '', code: "Backquote"},
+            1: {default:'1', shift:'!', altgr: '', shift_altgr: '', code: "Dight1"},
+            2: {default:'2', shift:'@', altgr: '', shift_altgr: '', code: "Dight2"},
+            3: {default:'3', shift:'#', altgr: '', shift_altgr: '', code: "Dight3"},
+            4: {default:'4', shift:'$', altgr: '', shift_altgr: '', code: "Dight4"},
+            5: {default:'5', shift:'%', altgr: '', shift_altgr: '', code: "Dight5"},
+            6: {default:'6', shift:'^', altgr: '', shift_altgr: '', code: "Dight6"},
+            7: {default:'7', shift:'&', altgr: '', shift_altgr: '', code: "Dight7"},
+            8: {default:'8', shift:'*', altgr: '', shift_altgr: '', code: "Dight8"},
+            9: {default:'9', shift:'(', altgr: '', shift_altgr: '', code: "Dight9"},
+            0: {default:'0', shift:')', altgr: '', shift_altgr: '', code: "Dight0"},
+            minus: {default:'-', shift:'_', altgr: '', shift_altgr: '', code: "Minus"},
+            eq: {default:'=', shift:'+', altgr: '', shift_altgr: '', code: "Equal"},
+            backspace: {default: '', type: 'bksp', width: 'bksp', code: "Backspace"},
+            tab: {default: 'Tab', type: 'tab', width: 'tab', code: "Tab"},
+            q: {default:'q', shift:'Q', altgr: '', shift_altgr: '', code: "KeyQ"},
+            w: {default:'w', shift:'W', altgr: '', shift_altgr: '', code: "KeyW"},
+            e: {default:'e', shift:'E', altgr: '', shift_altgr: '', code: "KeyE"},
+            r: {default:'r', shift:'R', altgr: '', shift_altgr: '', code: "KeyR"},
+            t: {default:'t', shift:'T', altgr: '', shift_altgr: '', code: "KeyT"},
+            y: {default:'y', shift:'Y', altgr: '', shift_altgr: '', code: "KeyY"},
+            u: {default:'u', shift:'U', altgr: '', shift_altgr: '', code: "KeyU"},
+            i: {default:'i', shift:'I', altgr: '', shift_altgr: '', code: "KeyI"},
+            o: {default:'o', shift:'O', altgr: '', shift_altgr: '', code: "KeyO"},
+            p: {default:'p', shift:'P', altgr: '', shift_altgr: '', code: "KeyP"},
+            open_square: {default:'[', shift:'{', altgr: '', shift_altgr: '', code: "BracketLeft"},
+            close_square: {default:']', shift:'}', altgr: '', shift_altgr: '', code: "BracketRight"},
+            backslash: {default:'\\', shift:'|', altgr: '', shift_altgr: '', code: "Backslash"},
+            caps: {default:'Caps Lock', type: 'caps', width:'caps', code: "CapsLock"},
+            s: {default:'s', shift:'S', altgr: '', shift_altgr: '', code: "KeyS"},
+            a: {default:'a', shift:'A', altgr: '', shift_altgr: '', code: "KeyA"},
+            d: {default:'d', shift:'D', altgr: '', shift_altgr: '', code: "KeyD"},
+            f: {default:'f', shift:'F', altgr: '', shift_altgr: '', code: "KeyF"},
+            g: {default:'g', shift:'G', altgr: '', shift_altgr: '', code: "KeyG"},
+            h: {default:'h', shift:'H', altgr: '', shift_altgr: '', code: "KeyH"},
+            j: {default:'j', shift:'J', altgr: '', shift_altgr: '', code: "KeyJ"},
+            k: {default:'k', shift:'K', altgr: '', shift_altgr: '', code: "KeyK"},
+            l: {default:'l', shift:'L', altgr: '', shift_altgr: '', code: "KeyL"},
+            semicolon: {default:';', shift:':', altgr: '', shift_altgr: '', code: "Semicolon"},
+            apostrophe: {default:'\'', shift:'\"', altgr: '', shift_altgr: '', code: "Quote"},
+            enter: {default:'Enter', type: 'enter', width:'enter', code: "Enter"},
+            shift: {default:'Shift', type: 'shift', width:'shift', code: "ShiftLeft"},
+            z: {default:'z', shift:'Z', altgr: '', shift_altgr: '', code: "KeyZ"},
+            x: {default:'x', shift:'X', altgr: '', shift_altgr: '', code: "KeyX"},
+            c: {default:'c', shift:'C', altgr: '', shift_altgr: '', code: "KeyC"},
+            v: {default:'v', shift:'V', altgr: '', shift_altgr: '', code: "KeyV"},
+            b: {default:'b', shift:'B', altgr: '', shift_altgr: '', code: "KeyB"},
+            n: {default:'n', shift:'N', altgr: '', shift_altgr: '', code: "KeyN"},
+            m: {default:'m', shift:'M', altgr: '', shift_altgr: '', code: "KeyM"},
+            up: {default:'', type: 'up', code: "ArrowUp"},
+            rshift: {default:'Shift', type: 'shift', code: "ShiftRight"},
+            comma: {default:',', shift:'<', altgr: '', shift_altgr: '', code: "Comma"},
+            period: {default:'.', shift:'>', altgr: '', shift_altgr: '', code: "Period"},
+            slash: {default:'/', shift:'?', altgr: '', shift_altgr: '', code: "Period"},
+            ctrl: {default:'ctrl', func:'ctrl', shift:'?', altgr: '', shift_altgr: '', code: "ControlLeft"},
+            os: {default:'os', func:'os', altgr: '', shift_altgr: '', code: "MetaLeft"},
+            alt: {default:'alt', func:'alt', altgr: '', shift_altgr: '', code: "AltLeft"},
+            space: {default:'space', func:'space', altgr: '', shift_altgr: '', width: "space", code: "Space"},
+            altgr: {default:'Alt Gr', func:'altgr', altgr: '', shift_altgr: '', code: "AltRight"},
+            rctrl: {default:'Alt Gr', func:'altgr', altgr: '', shift_altgr: '', code: "ControlRight"},
+            left: {default:'', type: 'left', code: "ArrowLeft"},
+            down: {default:'', type: 'down', code: "ArrowDown"},
+            right: {default:'', type: 'right', code: "ArrowRight"},
+
         }
     }
     keyboardMatrix() {
         return [['tilda', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'minus', 'eq', 'backspace'],
                 ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'open_square', 'close_square'],
                 ['caps','a','s', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'semicolon', 'apostrophe', 'enter'],
-                ['shift','z','x', 'c', 'v', 'b', 'n', 'm', 'comma', 'period', 'slash', 'shift'],
-                ['ctrl', 'os', 'alt', 'space', 'altgr', 'ctrl']]
+                ['shift','z','x', 'c', 'v', 'b', 'n', 'm', 'comma', 'period', 'slash', 'rshift'],
+                ['ctrl', 'os', 'alt', 'space', 'altgr', 'rctrl']]
     }
     generateMatrixKeyboard(matrix, layout) {
         let strHtml = `<div class="keyboard__inner">`
@@ -86,8 +93,8 @@ class Keyboard {
     }
     generateButtonHtml(key, layout) {
         console.log(key);
-        //if()
-        return `<button class="btn" data-type="sym" data-key="${key}" data-keyshift="${layout[key].shift}" data-keyaltgr="${layout[key].altgr}" data-corner="${layout[key].shift_altgr}">
+        const type = layout[key].type ? layout[key].type : "sym";
+        return `<button class="btn" data-type=${type} data-key="${key}" data-keyshift="${layout[key].shift}" data-keyaltgr="${layout[key].altgr}" data-corner="${layout[key].shift_altgr}">
                 ${layout[key].default}</button>`
     }
 }
@@ -101,9 +108,10 @@ function resetStat(){
     stat.ctrl = false;
     stat.alt = false;
 }
+
 const taCols = 100;
 const taRows = 20;
-//keyboard = new HTMLDivElement();
+
 document.addEventListener('keydown', function(event) {
     console.log(event.code);
   });
@@ -123,13 +131,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
         //textarea.value = textarea.value.substring(0,textarea.selectionStart-1) + textarea.value.substring(textarea.selectionStart,textarea.value.length);
         switch(typeInput){
             case 'sym':
-                textarea.value += kb.layout[val.dataset.key].default;
+                //textarea.value += kb.layout[val.dataset.key].default;
                 if(stat.shift) {
-                    textarea.value += kb.layout[val.dataset.shift];
-                } else if(stat.altgr) {
-                    textarea.value += kb.layout[val.dataset.altgr];
-                } else if(stat.altgr&&stat.shift) {
-                    textarea.value += kb.layout[val.dataset.shift_altgr];
+                    textarea.value += kb.layout[val.dataset.key].shift;
+                    stat.shift = false;
+                } else {
+                    textarea.value += kb.layout[val.dataset.key].shift;
                 }
                 break;
             case 'bskp':
